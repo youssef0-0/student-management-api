b05c03a5460f531e90fdf58b3e8b1dccdb7b93cb
youssef0-0 <magic.yosef111@gmail.com>
2026-07-18 21:41:58 +0300

    Update StudentsApplication.java

--- src/main/java/com/yusuf/students/StudentsApplication.java
+++ src/main/java/com/yusuf/students/StudentsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentsApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(StudentsApplication.class, args);
 	}
 
